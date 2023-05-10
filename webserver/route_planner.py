daa8f105e2cce7b36221aa01c2d6901657d2a951
isacbelfrage <72108699+isacbelfrage@users.noreply.github.com>
2023-05-10 13:59:23 +0200

    Update route_planner.py

--- webserver/route_planner.py
+++ webserver/route_planner.py
@@ -1,28 +1,36 @@
 from flask import Flask, request
+from geopy.geocoders import Nominatim
 from flask_cors import CORS
 import redis
+import json
+import subprocess
+
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
-app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
+redis_server = redis.Redis(host="localhost")
 
-# change this to connect to your redis server
-# ===============================================
-redis_server = redis.Redis("localhost")
-# ===============================================
+geolocator = Nominatim(user_agent="my_request")
+region = ", Lund, Skåne, Sweden"
 
-redis_server.set('longitude', 13.21008)
-redis_server.set('latitude', 55.71106)
+@app.route('/planner', methods=['POST'])
+def route_planner():
+    Addresses =  json.loads(request.data.decode())
+    ToAddress = Addresses['taddr']
 
-@app.route('/drone', methods=['POST'])
-def drone():
-    drone_location = request.get_json()
-    longitude = drone_location['longitude']
-    latitude = drone_location['latitude']
-    redis_server.set('longitude', longitude)
-    redis_server.set('latitude', latitude)
-    return 'Get data'
+    home_location = (float(redis_server.get('longitude')), float(redis_server.get('latitude')))
+    print(home_location)
+    to_location = geolocator.geocode(ToAddress + region)
+
+    if home_location is None:
+        message = 'Departure address not found, please input a correct address'
+    elif to_location is None:
+        message = 'Destination address not found, please input a correct address'
+    else:
+        message = 'Get address! Start moving'
+        # Skickar task till redis
+        redis_server.rpush('drone_tasks', json.dumps((to_location.longitude, to_location.latitude)))
+    return message
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port='5001')
-
+    app.run(debug=True, host='0.0.0.0', port='5002')
